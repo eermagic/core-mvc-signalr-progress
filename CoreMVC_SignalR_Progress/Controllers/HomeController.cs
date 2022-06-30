adecf3112e2d05d8c03c983ef7398c8bc724f65e
Mars <eermagic@yahoo.com.tw>
2022-06-30 23:41:44 +0800

    首次上傳

---
+++ CoreMVC_SignalR_Progress/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CoreMVC_SignalR_Progress.Hubs;
+using CoreMVC_SignalR_Progress.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections;
+using System.Diagnostics;
+
+namespace CoreMVC_SignalR_Progress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<ProgressHub> progressHubContext;
+
+        public HomeController(ILogger<HomeController> logger, IHubContext<ProgressHub> _hubContext)
+        {
+            _logger = logger;
+            progressHubContext = _hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 模擬會執行很久的範例
+        /// </summary>
+        /// <param name="inModel"></param>
+        /// <returns></returns>
+        public IActionResult StartProgress(Dictionary<string, string> inModel)
+        {
+            Dictionary<string, string> outModel = new Dictionary<string, string>();
+
+            int progress = 0;
+            //執行 10 秒
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(1000);//暫停1秒
+                progress = i * 10;
+                progressHubContext.Clients.Client(inModel["HubConnId"]).SendAsync("UpdProgress", progress);
+            }
+
+            outModel["ResultMsg"] = "執行完成";
+
+            return Json(outModel);
+        }
+    }
+}
---
+++ CoreMVC_SignalR_Progress/Hubs/ProgressHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CoreMVC_SignalR_Progress.Hubs
+{
+    public class ProgressHub : Hub
+    {
+        /// <summary>
+        /// 連線事件
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            // 回傳連線ID
+            await Clients.Client(Context.ConnectionId).SendAsync("SetHubConnId", Context.ConnectionId);
+
+            await base.OnConnectedAsync();
+        }
+    }
+}
